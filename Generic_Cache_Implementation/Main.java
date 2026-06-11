f50a45068614203c30bd9a7b4b1471910b429090
Lekesky <61468937+Lekesky@users.noreply.github.com>
2026-06-11 12:24:58 -0400

    Generic_Cache_Implementation Day1

---
+++ Generic_Cache_Implementation/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Cache<Integer> c = new Cache<>(5);
+        c.put(50);
+        c.put(40);
+        c.put(30);
+        c.put(20);
+        c.put(10);
+        c.put(1000);
+
+        c.print();
+        // int removedValue = c.evict(30);
+        // System.out.println("Remved the value " + removedValue + " from the cache");
+        // c.print();
+    }
+}
